e70419e8492a0fb0c4a7169db70b43cdfc41f262
Kristina Karantsevich <k.karantsevich@gmail.com>
2023-07-14 15:18:01 +0200

    feat: solve next 5 tasks

--- src/03-arrays-tasks.js
+++ src/03-arrays-tasks.js
@@ -271,8 +271,8 @@ function getSecondItems(arr) {
  *  [ 'a', 'b', 'c', null ] => [ 'a', 'b','b', 'c','c','c',  null,null,null,null ]
  *  [ 1,2,3,4,5 ] => [ 1, 2,2, 3,3,3, 4,4,4,4, 5,5,5,5,5 ]
  */
-function propagateItemsByPositionIndex(/* arr */) {
-  throw new Error('Not implemented');
+function propagateItemsByPositionIndex(arr) {
+  return arr.map((element, index) => Array(index + 1).fill(element, 0, index + 2)).flat();
 }
 
 
@@ -289,8 +289,8 @@ function propagateItemsByPositionIndex(/* arr */) {
  *   [ 1,2,3,4,5,6,7,8,9,10 ] => [ 10, 9, 8 ]
  *   [ 10, 10, 10, 10 ] => [ 10, 10, 10 ]
  */
-function get3TopItems(/* arr */) {
-  throw new Error('Not implemented');
+function get3TopItems(arr) {
+  return arr.sort((current, next) => current - next).slice(-3).reverse();
 }
 
 
@@ -307,8 +307,8 @@ function get3TopItems(/* arr */) {
  *   [ null, 1, 'elephant' ] => 1
  *   [ 1, '2' ] => 1
  */
-function getPositivesCount(/* arr */) {
-  throw new Error('Not implemented');
+function getPositivesCount(arr) {
+  return arr.filter((current) => typeof current === 'number' && current > 0).length;
 }
 
 /**
@@ -324,8 +324,19 @@ function getPositivesCount(/* arr */) {
  *   [ 'nine','eight','nine','eight'] => [ 'eight','eight','nine','nine']
  *   [ 'one','one','one','zero' ]     => [ 'zero','one','one','one' ]
  */
-function sortDigitNamesByNumericOrder(/* arr */) {
-  throw new Error('Not implemented');
+function sortDigitNamesByNumericOrder(arr) {
+  const map = new Map();
+  map.set('zero', 0);
+  map.set('one', 1);
+  map.set('two', 2);
+  map.set('three', 3);
+  map.set('four', 4);
+  map.set('five', 5);
+  map.set('six', 6);
+  map.set('seven', 7);
+  map.set('eight', 8);
+  map.set('nine', 9);
+  return arr.sort((current, next) => map.get(current) - map.get(next));
 }
 
 /**
