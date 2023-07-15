9e5196d1471bad628f419ff738cd3685aeced5fe
Kristina Karantsevich <k.karantsevich@gmail.com>
2023-07-15 11:16:55 +0200

    feat: solve next 3 tasks

--- src/03-arrays-tasks.js
+++ src/03-arrays-tasks.js
@@ -351,8 +351,9 @@ function sortDigitNamesByNumericOrder(arr) {
  *   [ -1, 1, -1, 1 ]      => 0
  *   [ 1, 10, 100, 1000 ]  => 1111
  */
-function getItemsSum(/* arr */) {
-  throw new Error('Not implemented');
+function getItemsSum(arr) {
+  const initialValue = 0;
+  return arr.reduce((accumulator, element) => accumulator + element, initialValue);
 }
 
 /**
@@ -367,8 +368,8 @@ function getItemsSum(/* arr */) {
  *  [ -1, 'false', null, 0 ] => 2
  *  [ null, undefined, NaN, false, 0, '' ]  => 6
  */
-function getFalsyValuesCount(/* arr */) {
-  throw new Error('Not implemented');
+function getFalsyValuesCount(arr) {
+  return arr.filter((element) => Boolean(element) === false).length;
 }
 
 /**
@@ -385,8 +386,8 @@ function getFalsyValuesCount(/* arr */) {
  *    [ null, undefined, null ], null => 2
  *    [ true, 0, 1, 'true' ], true => 1
  */
-function findAllOccurences(/* arr, item */) {
-  throw new Error('Not implemented');
+function findAllOccurences(arr, item) {
+  return arr.filter((element) => element === item).length;
 }
 
 /**
