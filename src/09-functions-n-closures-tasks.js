30ccaa02da4f0c21366478198e2dc2167039da77
Kristina Karantsevich <k.karantsevich@gmail.com>
2023-09-20 12:26:50 +0200

    feat: finish functions-n-closures-tasks

--- src/09-functions-n-closures-tasks.js
+++ src/09-functions-n-closures-tasks.js
@@ -194,8 +194,13 @@ function partialUsingArguments(fn, ...args1) {
  *   getId4() => 7
  *   getId10() => 11
  */
-function getIdGeneratorFunction(/* startFrom */) {
-  throw new Error('Not implemented');
+function getIdGeneratorFunction(startFrom) {
+  let counter = 0;
+  return () => {
+    const result = startFrom + counter;
+    counter += 1;
+    return result;
+  };
 }
 
 
